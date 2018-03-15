87a494eabc4a482a4d438c266f8c89d5a2033df0
Tomasz Mlodawski <tmlodawski@consol.pl>
2018-03-15 18:08:37 +0100

    first button

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text,TextInput, View } from 'react-native';
+import { StyleSheet, Text,TextInput, View ,Image, TouchableOpacity} from 'react-native';
 
 export default class App extends React.Component {
+    callFun = () =>
+    {
+
+        alert("Image Clicked!!!");
+
+    }
   render() {
     return (
       <View style={styles.container}>
+          <TouchableOpacity style={styles.AddStyle} activeOpacity={0.5} onPress={ this.callFun }>
+
+              <Image source={require('./images/add.png')}
+                  style={styles.ImageIconStyle}
+              />
+
+              <View style={styles.SeparatorLine} />
+
+              <Text style={styles.TextStyle}> Add recipe </Text>
+
+          </TouchableOpacity>
         <TextInput></TextInput>
+        <Text>HAHAH</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
@@ -15,9 +33,46 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+    AddStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'green',
+        borderWidth: .5,
+        borderColor: '#fff',
+        height: 40,
+        borderRadius: 5 ,
+        margin: 5,
+
+    },
+    ImageIconStyle: {
+        top: 5,
+        padding: 10,
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode : 'stretch',
+
+    },
+
+    TextStyle :{
+
+        color: "#fff",
+        marginBottom : 4,
+        marginRight :20,
+
+    },
+
+    SeparatorLine :{
+
+        backgroundColor : '#fff',
+        width: 1,
+        height: 40
+
+    }
 });
