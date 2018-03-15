cd34a6c72e53ac87127eb247afc868e5f7bb72eb
Tomasz Mlodawski <tmlodawski@consol.pl>
2018-03-15 20:12:06 +0100

    first button

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import axios from 'axios';
-import { AsyncStorage,StyleSheet, Text,TextInput, View ,Image, TouchableOpacity} from 'react-native';
+import {AsyncStorage, Image,Button, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 
 export default class App extends React.Component {
-    saveItem() {
+    constructor() {
+        super();
+        this.state =
+            {
+                TextInputValueHolder: 'a'
+            }
+    }
+    getAllData() {
         axios.get('https://community-food2fork.p.mashape.com/search', {
             params: {
                 key: '486ed0c9faa8176abb1bdbb8142feb92',
-                q: 'shredded chicken'
+                q: this.state.TextInputValueHolder.toString()
             },
             headers: {
                 'X-Mashape-Key': 'dNSOCbIg9hmshP8RMirNls4rJG11p1ZpVjDjsnU3Fout7Ze7Gu',
@@ -16,12 +23,10 @@ export default class App extends React.Component {
         }).then(response => {
             //this.props.stopFetchAction();
             console.log(response.data.recipes);
-           // this.props.newRecipeList(response.data.recipes);
+            // this.props.newRecipeList(response.data.recipes);
         });
-
-
-
-
+    }
+    saveItem() {
         const object = {
           name : 'scrambled',
           category : 'breakfast',
@@ -45,7 +50,12 @@ export default class App extends React.Component {
         alert("Image Clicked!!!");
 
     }
-  render() {
+
+
+
+
+
+    render() {
     return (
       <View style={styles.container}>
           <TouchableOpacity style={styles.AddStyle} activeOpacity={0.5} onPress={ this.saveItem}>
@@ -64,8 +74,16 @@ export default class App extends React.Component {
               Click me to display
             </Text>
           </TouchableOpacity>
-        <TextInput></TextInput>
-        <Text>HAHAH</Text>
+
+          <TextInput
+              underlineColorAndroid = "transparent"
+              placeholder="Enter Text Here To Share"
+              style = { styles.TextInputStyle }
+              onChangeText = { ( TextInputText ) => { this.setState({ TextInputValueHolder: TextInputText })} }
+          />
+          <Button title="Click Here To Share TextInput Inside Typed Text as Message" onPress={ this.getAllData() } />
+
+          <Text>HAHAH</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
@@ -101,7 +119,16 @@ const styles = StyleSheet.create({
         resizeMode : 'stretch',
 
     },
-
+    TextInputStyle:
+        {
+            borderWidth: 1,
+            borderColor: '#009688',
+            width: '100%',
+            height: 40,
+            borderRadius: 10,
+            marginBottom: 10,
+            textAlign: 'center'
+        },
     TextStyle :{
 
         color: "#fff",
