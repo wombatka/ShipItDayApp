8c460967963e61b4187fadb61b6dd3d210e9d214
Joanna Niechwiedowicz <joannan@consol.de>
2018-03-15 18:44:49 +0100

    async storege

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text,TextInput, View ,Image, TouchableOpacity} from 'react-native';
+import { AsyncStorage,StyleSheet, Text,TextInput, View ,Image, TouchableOpacity} from 'react-native';
 
 export default class App extends React.Component {
+    saveItem() {
+        let recipie = 'scramblked eggs';
+        AsyncStorage.setItem('recipie', recipie);
+    }
+    displayData = async()=>{
+      try{
+        let user = await AsyncStorage.getItem('user');
+          alert(user);
+      }catch(error){
+
+      }
+    }
     callFun = () =>
     {
 
@@ -11,7 +23,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-          <TouchableOpacity style={styles.AddStyle} activeOpacity={0.5} onPress={ this.callFun }>
+          <TouchableOpacity style={styles.AddStyle} activeOpacity={0.5} onPress={ this.saveItem }>
 
               <Image source={require('./images/add.png')}
                   style={styles.ImageIconStyle}
@@ -21,6 +33,11 @@ export default class App extends React.Component {
 
               <Text style={styles.TextStyle}> Add recipe </Text>
 
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.displayData}>
+            <Text>
+              Click me to save data
+            </Text>
           </TouchableOpacity>
         <TextInput></TextInput>
         <Text>HAHAH</Text>
@@ -33,46 +50,9 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
   },
-    AddStyle: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: 'green',
-        borderWidth: .5,
-        borderColor: '#fff',
-        height: 40,
-        borderRadius: 5 ,
-        margin: 5,
-
-    },
-    ImageIconStyle: {
-        top: 5,
-        padding: 10,
-        margin: 5,
-        height: 25,
-        width: 25,
-        resizeMode : 'stretch',
-
-    },
-
-    TextStyle :{
-
-        color: "#fff",
-        marginBottom : 4,
-        marginRight :20,
-
-    },
-
-    SeparatorLine :{
-
-        backgroundColor : '#fff',
-        width: 1,
-        height: 40
-
-    }
 });
